4c4a77ac5977d19de9bab149cee3c0bd7c6aba79
Hlaing Min Oo <52852283+Williamhmo@users.noreply.github.com>
2022-09-14 23:11:01 +0630

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def takePhoto():
         return img
     
 def detect(img):
-    model=keras.models.load_model("C:\\Users\\User\\anaconda3\\Simbolo\\project\\model1_maskdataall.h5")
+    model=keras.models.load_model("C:\\Users\\User\\anaconda3\\Simbolo\\project\\model3_maskdataall.h5")
     img=ImageOps.fit(img,(250,250),Image.ANTIALIAS)
     x = tf.keras.preprocessing.image.img_to_array(img)
     x = np.expand_dims(x,axis=0)
